dad15935f4799d951019deb72d07e1596e8378c3
florez <florez@ethz.ch>
2025-03-29 23:25:09 +0100

    style: themeing

--- bamboost_tui/app.py
+++ bamboost_tui/app.py
@@ -1,38 +1,36 @@
 from __future__ import annotations
 
-from textual import command
 from textual.app import App
 from textual.binding import Binding
 from textual.css.query import NoMatches
-from textual.screen import Screen
 from textual.theme import BUILTIN_THEMES, Theme
-from textual.widgets import HelpPanel, LoadingIndicator
+from textual.widgets import HelpPanel
 
 from bamboost_tui.collection_table import ScreenCollection
-
 
 ansi_theme = Theme(
     name="ansi",
     primary="ansi_blue",
-    secondary="ansi_cyan",
+    secondary="ansi_magenta",
     accent="ansi_yellow",
-    foreground="ansi_white",
+    foreground="ansi_bright_white",
     background="ansi_default",
-    success="ansi_green",
+    success="ansi_bright_green",
     warning=BUILTIN_THEMES["textual-dark"].warning,
     error="ansi_red",
     surface="ansi_black",
     panel="ansi_bright_black",
-    boost="ansi_green",
+    boost="ansi_bright_green",
     dark=True,
     variables={
+        "foreground-muted": "ansi_white",
         "input-cursor-background": "ansi_black",
         "input-cursor-foreground": "ansi_white",
         "block-cursor-background": "ansi_black",
         "block-cursor-foreground": "ansi_white",
         "border": "ansi_bright_black",
-        "border-focus": "ansi_white",
-        # "block-hover-background": "$surface",
+        "border-focus": "ansi_blue",
+        "footer-background": "ansi_black",
     },
 )
 
@@ -52,22 +50,21 @@ class BamboostApp(App):
     def on_mount(self) -> None:
         if ansi_colors_set := self.ansi_color:
             self.register_theme(ansi_theme)
-            self.ansi_theme.ansi_colors
             self.theme = "ansi"
             self.ansi_color = ansi_colors_set
         else:
-            self.theme = "nord"
+            self.theme = "gruvbox"
 
         # This fixes the bug that the screen is empty after resuming the app
         self.app_resume_signal.subscribe(self, lambda *_args, **_kwargs: self.refresh())
 
         self.push_screen(ScreenCollection())
 
-    def action_toggle_help_panel(self):
-        try:
-            self.query_one(HelpPanel).remove()
-        except NoMatches:
-            self.action_show_help_panel()
+    async def action_toggle_help_panel(self):
+         try:
+             await self.query_one(HelpPanel).remove()
+         except NoMatches:
+             self.action_show_help_panel()
 
     def action_pop_screen_or_exit(self) -> None:
         self.pop_screen()
