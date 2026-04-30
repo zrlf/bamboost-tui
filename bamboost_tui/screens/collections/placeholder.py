8d6b1b9cc83594021ae126a8d1cd75219759617e
dab-work <dbueeler@gmail.com>
2026-04-30 17:59:59 +0200

    fix(placeholder): fixed bug when the bindings where not processed at startup

--- bamboost_tui/screens/collections/placeholder.py
+++ bamboost_tui/screens/collections/placeholder.py
@@ -5,23 +5,13 @@ from textual.app import ComposeResult
 from textual.color import Color
 from textual.containers import Center
 from textual.widgets import Static
+from textual.reactive import reactive
 
 
 class Placeholder(Static):
+    binding_text = reactive("???")
+
     def compose(self) -> ComposeResult:
-        picker_binding = next(
-            (
-                b.binding
-                for b in self.app.active_bindings.values()
-                if b.binding.action == "toggle_picker"
-            ),
-            None,
-        )
-        key_name = (
-            picker_binding.key_display or picker_binding.key
-            if picker_binding
-            else "???"
-        )
         with Center():
             yield Static(
                 dedent("""
@@ -34,15 +24,37 @@ class Placeholder(Static):
                         """),
                 classes="logo",
             )
-        with Center():
-            val = self.app.theme_variables.get("secondary")
-            c = Color.parse(val).rich_color.name
-            yield Static(
-                Text.from_markup(
-                    f"No collection selected. Press [bold {c}]{key_name}[/bold {c}] to open the collection picker."
-                )
-            )
+        yield Center(Static(id="message-label"))
         with Center():
             val = self.app.theme_variables.get("panel")
             c = Color.parse(val).rich_color.name
             yield Static(Text("A creation of florez/zrlf ♥", style=f"italic {c}"))
+
+    def _get_instruction_text(self) -> Text:
+        val = self.app.theme_variables.get("secondary")
+        c = Color.parse(val).rich_color.name
+        return Text.from_markup(
+            f"No collection selected. Press [bold {c}]{self.binding_text}[/bold {c}] to open the collection picker."
+        )
+
+    def on_mount(self) -> None:
+        picker_binding = next(
+            (
+                b.binding
+                for b in self.app.active_bindings.values()
+                if b.binding.action == "toggle_picker"
+            ),
+            None,
+        )
+        if picker_binding:
+            self.binding_text = picker_binding.key_display or picker_binding.key
+
+        self._update_display()
+
+    def _update_display(self) -> None:
+        try:
+            self.query_one("#message-label", Static).update(self._get_instruction_text())
+        finally:
+            pass
+
+    def watch_binding_text(self, _) -> None: self._update_display()
