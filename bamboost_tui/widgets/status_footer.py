5be8031930eff0daa8429b0d80d35a9896321c33
dab-work <dbueeler@gmail.com>
2026-05-27 13:17:23 +0200

    enh(status): status footer is now only displayed if it has content

--- bamboost_tui/widgets/status_footer.py
+++ bamboost_tui/widgets/status_footer.py
@@ -1,13 +1,23 @@
 from textual.widgets import Static
+from textual.reactive import reactive
 
 from rich.console import RenderableType
 
 
 class StatusFooter(Static):
     """A Footer that also displays status update messages."""
+    has_content: reactive[bool] = reactive(False)
 
-    def display(self, content: RenderableType) -> None:
+    def on_mount(self) -> None:
+        self.visible = False
+
+    def display_status(self, content: RenderableType) -> None:
         self.update(content)
+        self.has_content = True
 
     def clear(self) -> None:
         self.update("")
+        self.has_content = False
+
+    def watch_has_content(self, has_content: bool) -> None:
+        self.visible = has_content
